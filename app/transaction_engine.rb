1ca090d039995878af41b2853617863c08e5d006
Graham Marlow <mgmarlow@gmail.com>
2021-05-29 12:42:03 -0700

    Implement undo

--- app/transaction_engine.rb
+++ app/transaction_engine.rb
@@ -4,7 +4,11 @@ class TransactionEngine
     @board_state = create_initial_board_state(args.state.entities)
 
     args.state.dispatch = ->(action) {
-      new_state = reduce(@board_state, action)
+      # TODO: compose reducers for different entities
+      # e.g. Player.reducer, Box.reducer
+      new_state = reduce(@board_state.dup, action)
+      return if new_state.to_s == @board_state.to_s
+
       @previous_states << @board_state.dup
       @board_state = new_state
     }
@@ -12,6 +16,11 @@ class TransactionEngine
 
   def tick(args)
     args.state.board_state = @board_state
+
+    if args.inputs.keyboard.key_down.z
+      return if @previous_states.empty?
+      @board_state = @previous_states.pop
+    end
   end
 
   # Serializable representation of game state
