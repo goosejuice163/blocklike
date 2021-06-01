867581cefde28c0e6c4e45eedccb5aa7e9178486
Graham Marlow <mgmarlow@gmail.com>
2021-05-31 21:01:05 -0700

    Add tileset

---
+++ assets/data/sokoban_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="sokoban_tileset" tilewidth="64" tileheight="64" tilecount="104" columns="13" objectalignment="topleft">
+ <image source="../img/sokoban_tilesheet.png" width="832" height="512"/>
+</tileset>
